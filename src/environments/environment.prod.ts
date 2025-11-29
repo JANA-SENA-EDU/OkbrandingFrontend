15d83c71b28088577666055e09967fd15bdf7b08
JANA-SENA-EDU <janavarrete32@soy.sena.edu.co>
2025-11-29 02:57:24 -0500

    To use production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://okbranding-ava4htfqc2ajefhh.chilecentral-01.azurewebsites.net', // URL de tu backend desplegado
+  apiUrl: 'https://okbranding-ava4htfqc2ajefhh.chilecentral-01.azurewebsites.net', // URL de tu backend desplegado en azure
 };
